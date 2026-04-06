a250e5b6ef23b4af0280c90911b87f488900acd8
Poornima kallam <145594732+s559222@users.noreply.github.com>
2026-04-06 14:02:44 -0500

    implemented costume list endpoint

--- app.js
+++ app.js
@@ -1,41 +1,87 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var Costume = require("./models/costume");
+var resourceRouter = require("./routes/resource");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const connectionString = process.env.MONGO_CON;
+mongoose.connect(connectionString);
+
+// Get the default connection
+var db = mongoose.connection;
+
+// Bind connection to error event
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function () {
+  console.log("Connection to DB succeeded");
+});
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/resource", resourceRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
+
+async function recreateDB() {
+  await Costume.deleteMany();
+
+  let instance1 = new Costume({
+    costume_type: "ghost",
+    size: "large",
+    cost: 15.4,
+  });
+  let instance2 = new Costume({
+    costume_type: "witch",
+    size: "medium",
+    cost: 20,
+  });
+  let instance3 = new Costume({
+    costume_type: "zombie",
+    size: "small",
+    cost: 10,
+  });
+
+  await instance1.save();
+  await instance2.save();
+  await instance3.save();
+
+  console.log("Database seeded");
+}
+
+let reseed = true;
+if (reseed) {
+  recreateDB();
+}
 
 module.exports = app;
---
+++ controllers/costume.js
@@ -0,0 +1,32 @@
+var Costume = require("../models/costume");
+
+// List of all Costumes
+exports.costume_list = async function (req, res) {
+  try {
+    let theCostumes = await Costume.find();
+    res.send(theCostumes);
+  } catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
+};
+
+// For a specific Costume
+exports.costume_detail = function (req, res) {
+  res.send("NOT IMPLEMENTED: Costume detail: " + req.params.id);
+};
+
+// Handle Costume create on POST
+exports.costume_create_post = function (req, res) {
+  res.send("NOT IMPLEMENTED: Costume create POST");
+};
+
+// Handle Costume delete on DELETE
+exports.costume_delete = function (req, res) {
+  res.send("NOT IMPLEMENTED: Costume delete DELETE " + req.params.id);
+};
+
+// Handle Costume update on PUT
+exports.costume_update_put = function (req, res) {
+  res.send("NOT IMPLEMENTED: Costume update PUT " + req.params.id);
+};
