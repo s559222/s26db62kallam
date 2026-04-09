448a86ff54749032596f57ec4bcaee1b44e53c62
Poornima kallam <145594732+s559222@users.noreply.github.com>
2026-04-09 13:34:30 -0500

    assigntmnet 12 part 1-2

--- controllers/costume.js
+++ controllers/costume.js
@@ -11,13 +11,21 @@ exports.costume_list = async function (req, res) {
   }
 };
 
+
 // For a specific Costume
-exports.costume_detail = function (req, res) {
-  res.send("NOT IMPLEMENTED: Costume detail: " + req.params.id);
+exports.costume_detail = async function (req, res) {
+  console.log("detail " + req.params.id);
+  try {
+    let result = await Costume.findById(req.params.id);
+    res.send(result);
+  } catch (err) {
+    res.status(500);
+    res.send(`{"error": "document for id ${req.params.id} not found"}`);
+  }
 };
 
 // Handle Costume create on POST
-// Handle Costume create on POST
+
 exports.costume_create_post = async function (req, res) {
   console.log(req.body);
   let document = new Costume();
@@ -40,8 +48,22 @@ exports.costume_delete = function (req, res) {
 };
 
 // Handle Costume update on PUT
-exports.costume_update_put = function (req, res) {
-  res.send("NOT IMPLEMENTED: Costume update PUT " + req.params.id);
+exports.costume_update_put = async function (req, res) {
+  console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`);
+  try {
+    let toUpdate = await Costume.findById(req.params.id);
+
+    if (req.body.costume_type) toUpdate.costume_type = req.body.costume_type;
+    if (req.body.cost) toUpdate.cost = req.body.cost;
+    if (req.body.size) toUpdate.size = req.body.size;
+
+    let result = await toUpdate.save();
+    console.log("Success " + result);
+    res.send(result);
+  } catch (err) {
+    res.status(500);
+    res.send(`{"error": "${err}: Update for id ${req.params.id} failed"}`);
+  }
 };
 
 // VIEWS
